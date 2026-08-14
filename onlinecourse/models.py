97541875f67dd04d9339dcb5448268146276ad9b
Le Hung <lequanghung2012@outlook.com>
2026-08-14 19:09:31 +0700

    Implement Submission, Question, and Choice models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -98,6 +98,45 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField('Choice')
+
+
+# Question Model
+
+# A Question model will save the questions of an exam with the following characteristics:
+# Used to persist questions for a course
+# Has a One-To-One relationship with the course
+# Has question text
+# Has a grade point for each question
+
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.question
+
+    # method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+
+# Choice Model
+
+# A Choice model saves all of the choices of a question:
+# One-To-One relationship with Question model
+# The choice text
+# Indicates if this choice is the correct one or not
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
